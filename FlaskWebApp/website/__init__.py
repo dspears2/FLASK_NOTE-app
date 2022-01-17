9e21c09d900128bc1238f193a67ca21fdb707637
DeMarco S <73176506+dspears2@users.noreply.github.com>
2022-01-16 19:01:49 -0600

    first commit

---
+++ FlaskWebApp/website/__init__.py
@@ -0,0 +1,61 @@
+import os
+from flask import Flask
+from os import path
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+#from flask_login import LoginManager
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "notes"
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'dev'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/notes'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from .models import db
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prex='/')
+    app.register_blueprint(auth, url_prex='/')
+
+    from .models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
